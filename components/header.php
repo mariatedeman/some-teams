15f9cb33e328ab3c81ee4aa4379ae4535c47db22
mariatedeman <m.tedeman@hotmail.com>
2025-10-01 14:33:30 +0200

    Första struktur HTML och CSS

---
+++ components/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="/styles/style.css">
+
+</head>
+
+<body>
+
+    <div class="header">
+
+        <img src="assets/logo-uwcl-white.webp">
+
+        <nav>
+            <a href="/index.php">Start</a>
+            <a href="/about.php">About</a>
+        </nav>
+    </div>
+
+    <main>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__ . '/components/header.php';
+
+?>
+
+<div class="one-clmn-grey">
+    <p>Presentation</p>
+</div>
+
+<div class="teams">
+    <div>
+        <h3>TEAM</h3>
+    </div>
+
+    <div>
+        <h3>TEAM</h3>
+    </div>
+
+    <div>
+        <h3>TEAM</h3>
+    </div>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+<?php
+
+require_once __DIR__ . '/components/footer.php';
